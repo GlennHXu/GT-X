d5434f62145fb35e557fa1e55fa1ddcce6bab791
glenn_pro <Glennxu2018@outlook.com>
2020-05-29 01:04:14 +0800

    取消了Typedef和Struct 的注释

--- Struct.cpp
+++ Struct.cpp
@@ -1,38 +1,38 @@
-//#define _CRT_SECUTE_NO_WARNINGS  // 规避strcpy()函数不安全问题
-//#define WIN32_LEAN_AND_MEAN 
-//#include<iostream>
-//#include<string>
-//using namespace std;
-//
-//struct student  // 定义结构体
-//{
-//	int num;
-//	char name[200];
-//	int age;
-//};
-//
-//// 声明函数
-//void f(struct student * pst);
-//void g(struct student * pst);
-//
-//int main()
-//{
-//	struct student st = { 100,"ldkjf",2334 };
-//	f(&st);
-//	g(&st);
-//
-//	system("pause");
-//	return 0;
-//}
-//
-//void f(struct student * pst)
-//{
-//	// 结构体指针的使用形式
-//	(*pst).num = 23;
-//	strcpy((*pst).name, "dkjfal");
-//	pst->age = 33;
-//}
-//void g(struct student * pst)
-//{
-//	cout << pst->num << " " << pst->name << " " << pst->age << endl;
-//}
+#define _CRT_SECUTE_NO_WARNINGS  // 规避strcpy()函数不安全问题
+#define WIN32_LEAN_AND_MEAN 
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct student  // 定义结构体
+{
+	int num;
+	char name[200];
+	int age;
+};
+
+// 声明函数
+void f(struct student * pst);
+void g(struct student * pst);
+
+int main()
+{
+	struct student st = { 100,"ldkjf",2334 };
+	f(&st);
+	g(&st);
+
+	system("pause");
+	return 0;
+}
+
+void f(struct student * pst)
+{
+	// 结构体指针的使用形式
+	(*pst).num = 23;
+	strcpy((*pst).name, "dkjfal");
+	pst->age = 33;
+}
+void g(struct student * pst)
+{
+	cout << pst->num << " " << pst->name << " " << pst->age << endl;
+}
--- Typedef.cpp
+++ Typedef.cpp
@@ -1,21 +1,21 @@
-//#include<iostream>
-//
-//using namespace std;
-//
-//typedef struct student
-//{
-//	int sid;
-//	char name[200];
-//	char sex;
-//
-//
-//}* PSTU, STU;  //等价于PSTU代表了struct student * ，STU代表了struct student;
-//int main(void)
-//{
-//	STU st;  //等价于struct stdent st;
-//	PSTU ps = &st;  //等价于struct stdent * ps =*st:
-//	ps->sid = 99;
-//	cout << ps->sid << endl;
-//	return 0;
-//
-//}
+#include<iostream>
+
+using namespace std;
+
+typedef struct student
+{
+	int sid;
+	char name[200];
+	char sex;
+
+
+}* PSTU, STU;  //等价于PSTU代表了struct student * ，STU代表了struct student;
+int main(void)
+{
+	STU st;  //等价于struct stdent st;
+	PSTU ps = &st;  //等价于struct stdent * ps =*st:
+	ps->sid = 99;
+	cout << ps->sid << endl;
+	return 0;
+
+}
