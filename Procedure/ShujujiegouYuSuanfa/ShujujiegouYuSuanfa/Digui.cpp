df33e8f059a99000fe3ebae9f03f92f583cd8cac
glennHXu <glennxu2018@outlook.com>
2020-07-22 00:38:28 +0800

    Digui.cpp

--- Procedure/ShujujiegouYuSuanfa/ShujujiegouYuSuanfa/Digui.cpp
+++ Procedure/ShujujiegouYuSuanfa/ShujujiegouYuSuanfa/Digui.cpp
@@ -12,13 +12,33 @@ long f(long n)
 		return 1;
 	else
 		return f(n - 1)*n;
-
+	
 }
+// 求解前100得和
+long sum(int n)
+{
+	if (1 == n)
+		return 1;
+	else
+		return n + sum(n - 1);
 
+	/* 使用循环
+	long s = 0;
+	int i;
+
+	for (i = 1; i <= 100; ++i)
+		s += i;
+		return s;
+	*/
+}
 
 int main(void)
 {
 	
 	cout << f(10) << endl;
+	cout << sum(100) << endl;
+	
 		return 0;
 }
+
+
